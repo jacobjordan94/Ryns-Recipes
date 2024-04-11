dbd864a8e7f0ae90e96d031c30ca1d35dd1b6632
jacobjordan94 <jacobjordan94@live.com>
2024-04-10 23:23:43 -0500

    add hover effects to cards

--- src/app/core/components/ingredient-card/ingredient-card.ts
+++ src/app/core/components/ingredient-card/ingredient-card.ts
@@ -1,6 +1,6 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, HostListener, Input, OnInit } from "@angular/core";
 import { Category, Ingredient, SubCategory } from "../../services/ingredients/ingredients.interface";
-import { Observable, tap } from "rxjs";
+import { Observable } from "rxjs";
 import { IngredientsService } from "../../services/ingredients/ingredients.service";
 import { AsyncPipe, NgIf } from "@angular/common";
 
@@ -9,7 +9,7 @@ import { AsyncPipe, NgIf } from "@angular/common";
     standalone: true,
     imports: [ NgIf, AsyncPipe ],
     template: `
-        <div class="ingredient-card card">
+        <div class="ingredient-card card" [class.shadow]="hovering && hoverable" [class.hover]="hovering && hoverable">
             <div class="card-body">
                 <h5 class="card-title">{{ ingredient.name }}</h5>
                 <h6 *ngIf="category$ | async as cat" class="card-subtitle mb-2 text-muted">
@@ -19,10 +19,25 @@ import { AsyncPipe, NgIf } from "@angular/common";
             </div>
         </div>
     `,
+    styles: [
+        '.ingredient-card { transition: 250ms ease-in-out; }',
+        '.ingredient-card.hover { cursor: pointer; transform: scale(1.015); }'
+    ]
 })
 export class AppIngredientCard implements OnInit {
 
+    public hovering = false;
+    @HostListener('mouseenter') 
+    mouseenter() {
+        this.hovering = true;
+    }
+    @HostListener('mouseleave')
+    mouseleave() {
+        this.hovering = false;
+    }
+
     @Input() public ingredient!: Ingredient;
+    @Input() public hoverable = false;
 
     category$!: Observable<Category | undefined>;
     subCategory$!: Observable<SubCategory | undefined>;
@@ -30,7 +45,6 @@ export class AppIngredientCard implements OnInit {
     constructor(private _is: IngredientsService) {}
 
     ngOnInit(): void {
-        console.log(this.ingredient);
         this.category$ = this._is.getCategory(this.ingredient.categoryID);
         this.subCategory$ = this._is.getSubCategoryById(this.ingredient.subCategoryId);    
     }
