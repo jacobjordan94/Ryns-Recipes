4165f7843198d17d16a760a619906c4f3cb506ad
jacobjordan94 <jacobjordan94@live.com>
2024-04-10 19:21:52 -0500

    add bootstrap icons

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -12,7 +12,10 @@ type HeaderLink = {
     imports: [ RouterLink, RouterLinkActive, NgFor ],
     template: `
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary c-light ps-3 pe-3">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">
+                <i class="bi bi-egg-fill"></i>
+                <span class="ms-2">Ryn's Recipes</span>
+            </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
