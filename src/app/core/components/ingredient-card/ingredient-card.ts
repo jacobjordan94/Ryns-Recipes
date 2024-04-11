f043d6d7158f2517f625e32b556c070965715b9c
jacobjordan94 <jacobjordan94@live.com>
2024-04-11 17:17:58 -0500

    add bookmarking capability

--- src/app/core/components/ingredient-card/ingredient-card.ts
+++ src/app/core/components/ingredient-card/ingredient-card.ts
@@ -2,19 +2,22 @@ import { Component, HostListener, Input, OnInit } from "@angular/core";
 import { Category, Ingredient, SubCategory } from "../../services/ingredients/ingredients.interface";
 import { Observable, map } from "rxjs";
 import { IngredientsService } from "../../services/ingredients/ingredients.service";
-import { AsyncPipe, NgIf } from "@angular/common";
+import { AsyncPipe, NgClass, NgIf } from "@angular/common";
 import { AppCategoryNameComponent } from "../category-name/category-name.component";
 import { AppSubCategoryNameComponent } from "../subcategory-name/subcategory-name.component";
+import { BookmarksService } from "../../services/bookmarks/bookmarks.service";
 
 @Component({
     selector: 'app-ingredient-card',
     standalone: true,
-    imports: [ NgIf, AsyncPipe, AppCategoryNameComponent, AppSubCategoryNameComponent ],
+    imports: [ NgIf, AsyncPipe, AppCategoryNameComponent, AppSubCategoryNameComponent, NgClass ],
     templateUrl: './ingredient-card.component.html',
     styles: [
-        '.ingredient-card { transition: 250ms ease-in-out; }',
-        '.ingredient-card.hover { cursor: pointer; transform: scale(1.015); }',
-        '.card-img-top { object-fit: cover; }'
+        '.ingredient-card { transition: 250ms ease-in-out; position: relative; }',
+        '.ingredient-card.hover, .btn-bookmark:hover { cursor: pointer; transform: scale(1.015); }',
+        '.card-img-top { object-fit: cover; }',
+        '.btn { width: min-content; }',
+        '.btn-bookmark { position: absolute; top: 12px; left: 12px; }'
     ]
 })
 export class AppIngredientCard implements OnInit {
@@ -31,15 +34,26 @@ export class AppIngredientCard implements OnInit {
 
     @Input() public ingredient!: Ingredient;
     @Input() public hoverable = false;
+    @Input() public showBookmark = false;
 
     category$!: Observable<Category | undefined>;
     hasSubCategory$!: Observable<boolean>;
+    isBookmarked$!: Observable<boolean>;
 
-    constructor(private _is: IngredientsService) {}
+    constructor(
+        private _is: IngredientsService, 
+        private _bs: BookmarksService
+    ) {}
 
     ngOnInit(): void {
         this.category$ = this._is.getCategory(this.ingredient.categoryID);
         this.hasSubCategory$ = this._is.getSubCategoryById(this.ingredient.subCategoryId).pipe(map(Boolean));
+        this.isBookmarked$ = this._bs.isBookmarkedIngredient(this.ingredient.id);
+    }
+
+    toggleBookmark(event: MouseEvent) {
+        event.stopPropagation();
+        this._bs.toggleIngredient(this.ingredient.id);
     }
 
 }
--- src/app/core/services/bookmarks/bookmarks.service.ts
+++ src/app/core/services/bookmarks/bookmarks.service.ts
@@ -7,6 +7,7 @@ import { Ingredient } from "../ingredients/ingredients.interface";
 export class BookmarksService {
 
     private _bookmarkedIngredientsIds$: BehaviorSubject<number[]> = new BehaviorSubject([2]);
+    public bookmarkedIngredientsIds$: Observable<number[]> = this._bookmarkedIngredientsIds$.asObservable();
     public bookmarkedIngredients$: Observable<Ingredient[]> = combineLatest([this._bookmarkedIngredientsIds$, this._is.ingredient$])
         .pipe(
             map(([bookmarkedIds, allIgredients]) => {
@@ -16,15 +17,30 @@ export class BookmarksService {
 
     constructor(private _is: IngredientsService) {}
 
-    addIngredient(ingredientId: number) {
+    public addIngredient(ingredientId: number) {
         const ids = this._bookmarkedIngredientsIds$.getValue();
         this._bookmarkedIngredientsIds$.next([...ids, ingredientId]);
     }
 
-    removeIngredient(ingredientId: number) {
+    public removeIngredient(ingredientId: number) {
         const ingredients = this._bookmarkedIngredientsIds$.getValue();
         const index = ingredients.findIndex(ingId => ingId === ingredientId);
         ingredients.splice(index, 1);
         this._bookmarkedIngredientsIds$.next([...ingredients]);
+    }
+
+    public isBookmarkedIngredient(ingredientId: number): Observable<boolean> {
+        return this._bookmarkedIngredientsIds$.pipe(
+            map(ids => ids.includes(ingredientId))
+        );
+    }
+
+    public toggleIngredient(ingredientId: number) {
+        const ingredientIds = this._bookmarkedIngredientsIds$.getValue();
+        if(ingredientIds.includes(ingredientId)) {
+            this.removeIngredient(ingredientId);
+        } else {
+            this.addIngredient(ingredientId);
+        }
     }
 }
