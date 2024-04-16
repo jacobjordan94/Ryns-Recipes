f145273c25d6eae95adf0100fac767c05c9326f0
Jacob <jacobjordan94@live.com>
2024-04-15 23:54:14 -0500

    add onerror and onload emitters to default-img directive; add hide on error functionality

--- src/app/core/directives/default-img/default-img.directive.ts
+++ src/app/core/directives/default-img/default-img.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Input, HostBinding, ElementRef, OnDestroy, HostListener } from '@angular/core';
+import { Directive, Input, HostBinding, ElementRef, OnDestroy, HostListener, Output, EventEmitter } from '@angular/core';
 import { Observable, Subject, debounceTime, skip, startWith, takeUntil } from 'rxjs';
 
 @Directive({
@@ -12,25 +12,38 @@ export class DefaultImgDirective implements OnDestroy {
   
   @Input() default!: string;
   
-  @HostBinding('class.image-loaded') hasLoaded = false;
+  @Input() hideOnFail = false;
+  @HostBinding('class.d-none') public hide: boolean = false;
+  private _failed: boolean = false;
+  
+  @HostBinding('class.image-loaded') hasLoaded: boolean = false;
 
   private _onDestroy$ = new Subject<void>();
 
+  @Output() public onError: EventEmitter<Event> = new EventEmitter();
+  @Output() public onLoad: EventEmitter<string> = new EventEmitter();
+
   constructor(private _el: ElementRef<HTMLImageElement>) {}
 
-  @HostListener('error')
-  public updateUrl() {
+  @HostListener('error', ['$event'])
+  public updateUrl(event: Event) {
+    this._failed = true;
+    if(this.hideOnFail) {
+      this.hide = true;
+    }
     this._getResizeObservable()
       .pipe(
         takeUntil(this._onDestroy$),
         debounceTime(350),
 
       ).subscribe(() => this._setDefault());
+    this.onError.emit(event);
   }
 
   @HostListener('load')
   public load() {
     this.hasLoaded = true;
+    this.onLoad.emit(this.src);
   }
 
   private _setDefault() {
