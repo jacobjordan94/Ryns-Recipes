4eca89f63b6335fae2d736b2b69f9bce78bc52ac
null <null>
2024-07-18 17:05:32 -0500

    update submit-recipe to show submitted ingredients

--- src/app/modules/submit-recipe/submit-recipe.component.ts
+++ src/app/modules/submit-recipe/submit-recipe.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { Ingredient } from '../../core/services/ingredients/ingredients.interface';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BehaviorSubject, Observable, Subscription, combineLatest, map, switchMap, take, tap } from 'rxjs';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { AddIngredientFormComponent } from './add-ingredient-form/add-ingredient-form.component';
+import { IngredientsService } from '../../core/services/ingredients/ingredients.service';
+import { Ingredient } from '../../core/services/ingredients/ingredients.interface';
 
 @Component({
   selector: 'app-submit-recipe',
@@ -13,17 +14,25 @@ import { AddIngredientFormComponent } from './add-ingredient-form/add-ingredient
   styleUrl: './submit-recipe.component.scss'
 })
 export class SubmitRecipeComponent {
-  recipeIngredients: Ingredient[] | undefined = [];
-  _recipeIngredients$: BehaviorSubject<Ingredient[] | undefined> = new BehaviorSubject(this.recipeIngredients);
-  recipeIngredients$: Observable<Ingredient[] | undefined> = this._recipeIngredients$.asObservable();
 
-  public addIngredient(newIngredient: Ingredient): void {
-    if (newIngredient){
-      this.recipeIngredients?.push(newIngredient);
-      console.log("newly added ingredient: ", newIngredient);
-      console.log("all ingredients: ", this.recipeIngredients)
-    }
+  _ingredientIds$: BehaviorSubject<number[]> = new BehaviorSubject([] as number[]);
+  ingredientIds$: Observable<number[]> = this._ingredientIds$.asObservable();
 
-    else (console.log("Undefined Ingredient"))
+  public recipeIngredients$: Observable<Ingredient[]> = this.ingredientIds$.pipe(
+    map(ingredientIds => ingredientIds.map(id => this._is.getIngredient(id) as Observable<Ingredient>)),
+    switchMap(ingredients => combineLatest(ingredients)),
+  );
+
+  constructor(private _is: IngredientsService) {}
+
+  public addIngredient(newIngredientId: number): Subscription {
+    return this.ingredientIds$.pipe(
+      take(1)
+    ).subscribe(ingredients => {
+      if(newIngredientId !== 0 && !newIngredientId) return;
+      ingredients?.push(newIngredientId);
+      this._ingredientIds$.next([...ingredients]);
+    });
   }
+  
 }
