a2e6a268a96204ab774c36ea161de351576947a3
null <null>
2024-07-18 17:03:17 -0500

    update forms to work better

--- src/app/modules/submit-recipe/add-ingredient-form/add-ingredient-form.component.ts
+++ src/app/modules/submit-recipe/add-ingredient-form/add-ingredient-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { ReactiveFormsModule, FormControl, FormGroup, Validators, FormsModule } from '@angular/forms';
 import { IngredientsService } from '../../../core/services/ingredients/ingredients.service';
-import { combineLatest, Observable, shareReplay, startWith, switchMap, tap } from 'rxjs';
+import { combineLatest, Observable, shareReplay, startWith, switchMap, take, tap } from 'rxjs';
 import { Category, Ingredient, SubCategory } from '../../../core/services/ingredients/ingredients.interface';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { IngredientFormComponent } from '../../submit-ingredient/ingredient-form/ingredient-form.component';
@@ -22,6 +22,7 @@ export class AddIngredientFormComponent {
   public addIngredientForm: FormGroup = new FormGroup({
     categoryId: new FormControl(0, [Validators.required]),
     subCategoryId: new FormControl(0, [Validators.required]),
+    ingredient: new FormControl(0, [ Validators.required ]),
   })
 
   private get _categoryIdForm(): FormControl<number> {
@@ -32,9 +33,12 @@ export class AddIngredientFormComponent {
     return this.addIngredientForm.get('subCategoryId') as FormControl<number>;
   }
 
-  constructor(private _is: IngredientsService) {
-    this.categories$ = this._is.categories$;
+  private get _ingredientForm(): FormControl<number | undefined> {
+    return this.addIngredientForm.get('ingredient') as FormControl<number>;
+  }
 
+  constructor(private _is: IngredientsService) {
+    this.categories$ = this._is.categories$.pipe(take(1));
     this.subcategory$ = this.addIngredientForm.get('categoryId')!.valueChanges.pipe(
       startWith(this._categoryIdForm.value),
       switchMap(categoryId => this._is.getSubCategoryByCategory(categoryId)),
@@ -47,7 +51,7 @@ export class AddIngredientFormComponent {
           this._subCategoryIdForm.patchValue(0);
           this._subCategoryIdForm.enable();
         }
-      })
+      }),
     )
 
     this.ingredients$ = combineLatest([
@@ -55,21 +59,24 @@ export class AddIngredientFormComponent {
       this._subCategoryIdForm.valueChanges.pipe(startWith(this._subCategoryIdForm.value))
     ]).pipe(
       switchMap(([categoryId, subCategoryId]) => this._is.getIngredientById(categoryId, subCategoryId)),
-      tap(ingredient => {
-        if (ingredient && ingredient.length > 0) {
-          console.log(ingredient);
-        }
-        else (console.log("ingredient undefined"))
-      })    )
+    );
+
+    this._categoryIdForm.valueChanges.subscribe(_catId => {
+      this._subCategoryIdForm.patchValue(undefined);
+      this._ingredientForm.patchValue(undefined);
+    });
+
+    this._subCategoryIdForm.valueChanges.subscribe(() => {
+      this._ingredientForm.patchValue(undefined);
+    });
+
+    this.addIngredientForm.valueChanges.subscribe(changes => console.log('changes', changes));
   };
 
-  @Output() public onSubmit: EventEmitter<Ingredient> = new EventEmitter();
+  @Output() public onSubmit: EventEmitter<number> = new EventEmitter();
 
-  submit(ing: Ingredient | String): void {
-    if (typeof ing === 'string') {
-      console.log(ing);
-    } else {
-      this.onSubmit.emit(ing as Ingredient)
-    }
+  submit(): void {
+    if(!this.addIngredientForm.valid) return;
+    this.onSubmit.emit(this.addIngredientForm.value['ingredient']);
   }
 }
